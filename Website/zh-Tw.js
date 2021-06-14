71f8080f5702cc274d6028d028f0d840697533ba
kami-sqmf <kamisqmf@gmail.com>
2021-06-14 16:46:43 +0800

    Add support for Traditional Chinese

---
+++ Website/zh-Tw.js
@@ -0,0 +1,180 @@
+exports.LANGUAGE = {
+  CODE: "zh-Hans",
+  STANDARD_CODE: "zh-Hans",
+  FULL_CODE: "正體中文",
+  WIKI_CODE: "en", // No Traditional Chinese Page in Wiki
+  CONTRIBUTORS: ["iLxlo_", "Kami-Sqmf"]
+};
+
+exports.NAV_BAR = {
+  HOME: "主頁",
+  BLOG: "部落格",
+  DONATE: "捐款",
+  WIKI: "指南",
+  DISCORD_SERVER: "Discord伺服器",
+  INVITE: "邀請",
+  VOTE: "投票",
+  STATUS: "狀態",
+  TERMS: "條款"
+};
+
+exports.HEADER = {
+  TITLE: "強大且多功能的 Discord 機器人",
+  SUBTITLE: "多語言且高自訂性，檢舉、日誌、歡迎和離開訊息、人數統計、防其他伺服器的邀請連結、自動審核、防止停權者破解，以及更多.....",
+  FEATURES: "功能",
+  INVITE: "邀請"
+};
+
+exports.FEATURES = {
+  TITLE: "功能",
+  SUBTITLE: "Filo 最棒的功能！",
+  MULTILANGUAGE: {
+    TITLE: "多語言",
+    DESCRIPTION: "我們相信提供您使用的語言會是最好的服務之一",
+    MORE_INFORMATION: "更多敘述"
+  },
+  ADMINISTRATION: {
+    TITLE: "成員管理",
+    DESCRIPTION: "這可說是本機器人最物超所值的地方",
+    ITEMS: {
+      WELCOMES: "歡迎訊息",
+      FAREWELLS: "離開訊息",
+      REPORTS: "舉報系統",
+      LOGGING: "日誌系統"
+    }
+  },
+  MODERATION: {
+    TITLE: "小幫手制度",
+    DESCRIPTION: "用幾個簡單的步驟讓您可以更快的選出小幫手。 是時候教訓那些破壞規矩的人了。 Filo 擁有這些指令:",
+  },
+  MEMBER_COUNTER: {
+    TITLE: "人數統計",
+    DESCRIPTION: "這個功能可以顯示出伺服器的人數。 只需要創一個頻道就好了",
+    MORE_INFORMATION: "更多敘述"
+  },
+  FUN: {
+    TITLE: "娛樂功能",
+    DESCRIPTION: "每個人都會無聊，所以 Filo 也想讓您身心舒暢一下. Filo 擁有",
+    ITEMS: {
+      FUN: "娛樂指令",
+      GAMES: "更多的遊戲指令"
+    }
+  },
+  ANIME: {
+    TITLE: "動漫",
+    PRE_DESCRIPTION: "我宅我驕傲!",
+    PRE_DESCRIPTION_2: "~ 對不起說錯了 ~",
+    DESCRIPTION: "Filo 有各式各樣的指令，像是",
+    AND: "和",
+    DESCRIPTION_2: "用這些指令來幫助您服務器內的宅宅們吧！",
+    ITEMS: {
+      ANIME: "動漫",
+      REACTION: "視覺化反應效果",
+      INTERACTION: "視覺化互動效果"
+    }
+  },
+  GAME_STATISTICS: {
+    TITLE: "遊戲統計數據",
+    DESCRIPTION: "如果您的社群對電腦遊戲對遊戲有興趣，別再浪費時間多想，Filo 可以滿足您",
+    ITEMS: {
+      GAME_STATISTICS: "遊戲統計數據的指令"
+    }
+  },
+  MISCELLANEOUS: {
+    TITLE: "Filo 小工具",
+    DESCRIPTION: "Filo 擁有各種各樣的指令，邀請至伺服器探索更多吧",
+    MORE_INFORMATION: "更多敘述"
+  }
+};
+
+exports.STATISTICS = {
+  TITLE: "目前 Filo's 的版圖",
+  GUILDS: "伺服器",
+  USERS: "使用者"
+};
+
+exports.DONATE = {
+  TITLE: "捐款給我們",
+  SUBTITLE: "幫助 Filo 繼續維持成長也需要您的支持",
+  TERMS_AND_CONDITIONS_WARNING: {
+    DESCRIPTION: "捐款時，即代表您已同意 Filo 的捐贈條款及細則",
+    MORE_INFORMATION: "更多敘述"
+  },
+  PLANS: {
+    PER_MONTH: "每月",
+    VAT_NOT_INCLUDED: "不含稅",
+    SUBSCRIBE: "訂閱",
+    PREVIOUS_REWARD: "本訂閱會包含前面等級的所有福利",
+    ROLE_REWARD: "伺服器的專屬徽章",
+    SUPPORT_REWARD: "展示出您對 Filo 的支持",
+    NICKNAME_REWARD: "在 Filo 官方伺服器更改暱稱",
+    EMOJIS_REWARD: "在我們的伺服器使用外部表情符號",
+    SOON_MORE: "即將到來 Soon™"
+  }
+};
+
+exports.REVIEW = {
+  TITLE: "留個評論給我們吧！",
+  SUBTITLE: "顯示您對 Filo 有著多少的好感"
+};
+
+exports.VOTE = {
+  TITLE: "為 Filo 投下寶貴的一票",
+  SUBTITLE: "每日投票即可獲得大量投票獎勵",
+  REWARDS_WARNING: {
+    DESCRIPTION: "您得先加入我們的伺服器才能領取獎勵",
+    JOIN_NOW: "現在加入!"
+  },
+  PAGE: "投票網站",
+  PLANS: {
+    PRE_TITLE: "投票獎勵",
+    VOTES: "累積的投票數",
+    PER_MONTH: "每月",
+    CLAIM_NOW: "現在領取",
+    PREVIOUS_REWARD: "前面等級的所有獎勵",
+    ROLE_REWARD: "獨特的身分組",
+    SUPPORT_REWARD: "展示出您對 Filo 的支持",
+    NICKNAME_REWARD: "在 Filo 官方伺服器更改暱稱",
+    EMOJIS_REWARD: "在我們的伺服器使用外部表情符號",
+    SOON_MORE: "即將到來 Soon™"
+  }
+};
+
+exports.FRIENDLY_WEBSITES = {
+  TITLE: "友情網站",
+  SUBTITLE: "他們都是讓 Filo 壯大的網站"
+};
+
+exports.READY = {
+  TITLE: "準備加入 Filo 了嗎?",
+  SUBTITLE: "加入超過 {guilds} 加入 10,000 多個信任 Filo 的伺服器！ !",
+  INVITE: "邀請",
+  GETTING_STARTED: "入門指南"
+};
+
+exports.FOOTER = {
+  HELPFUL_LINKS: {
+    TITLE: "實用連結",
+    WIKI: "指南",
+    DONATE: "捐款",
+    STATUS: "狀態"
+  },
+  IMPORTANT_LINKS: {
+    TITLE: "公司",
+    TERMS_AND_CONDITIONS: "條款及細則",
+    PRIVACY_POLICY: "隱私政策",
+    DMCA: "DMCA"
+  },
+  ABOUT_FILO: {
+    TITLE: "關於 Filo",
+    DESCRIPTION: "Filo 是一個功能強大且多用途的 Discord 機器人，自 2019 年以來就一直努力不懈的在 Discord 伺服器上服務。她擁有廣泛的管理和審核模塊，讓您工作更輕鬆。"
+  }
+};
+
+exports.THANKS = {
+  PAGE: {
+    TITLE: "感謝您邀請 Filo!",
+    DESCRIPTION: "我們很高興你相信 Filo。",
+    DESCRIPTION_2: "您將被重新導向到邀請頁面."
+  }
+}
