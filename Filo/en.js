c56320177d8fded982b00748343e86720090b681
iLxlo_ <garrulo.servi9@gmail.com>
2021-07-29 12:06:07 +0200

    Added translations of time and the clear command

--- Filo/en.js
+++ Filo/en.js
@@ -13,15 +13,20 @@ exports.ERROR_MESSAGES = {
   ALTERNATIVE_PERMISSION: "Alternative permission",
   ALTERNATIVE_PERMISSIONS: "Alternative permissions",
   MISSING_CLIENT_PERMISSION: "This error has been caused because I don't have the [``{{permission}}``]({{url}}) permission.",
-  MISSING_USER_PERMISSION: "This error has been caused because you don't have the [``{{permission}}``]({{url}}) permission.",
+  MISSING_USER_PERMISSION: "This error has been caused because you don't have the ``{{permission}}`` permission.",
   MISSING_USER_PERMISSION_2: "You don't have permissions to carry out this action with **{{user}}** ({{user.id}}).",
-
   INVALID_MEMBER: "The member you specified is invalid.",
   UNKNOWN_MEMBER: "The member you specified doesn't exist or isn't on the server.",
   INVALID_ROLE: "The role you have specified is invalid.",
   UNKNOWN_ROLE: "The role you specified doesn't exist.",
   MANAGED_ROLE: "The role you have specified is managed by an integration.",
-  MEMBER_ALREADY_HAS_THE_ROLE: "**{{user}}** ({{user.id}}) already has the {{role}} role assigned."
+  MEMBER_ALREADY_HAS_THE_ROLE: "**{{user}}** ({{user.id}}) already has the {{role}} role assigned.",
+  INVALID_NUMBER: "The number you have specified is invalid.",
+  MAX_NUMBER_REACHED: "The number you specified is greater than **{{number}}**.",
+  MIN_NUMBER_REACHED: "The number you specified is less than **{{number}}**.",
+  INVALID_CHANNEL: "The channel you have specified is invalid.",
+  UNKNOWN_CHANNEL: "The channel you specified doesn't exist.",
+  INVALID_CHANNEL_TYPE: "This action does not support the type of the channel that you specified. ({{valid_types}})"
 };
 
 exports.AUDIT_LOGS = {
@@ -32,10 +37,29 @@ exports.AUDIT_LOGS = {
   }
 };
 
+exports.TIME = {
+  SECOND: "Second",
+  SECONDS: "Seconds",
+  MINUTE: "Minute",
+  MINUTES: "Minutes",
+  HOUR: "Hour",
+  HOURS: "Hours",
+  DAY: "Day",
+  DAYS: "Days",
+  WEEK: "Week",
+  WEEKS: "Weeks",
+  MONTH: "Month",
+  MONTHS: "Months",
+  YEAR: "Year",
+  YEARS: "Years"
+};
+
 exports.GENERAL = {
   EXAMPLE: "Example",
   ADDED: "Added",
-  COMMAND: "Command"
+  COMMAND: "Command",
+  OR: "Or",
+  COOLDOWN: "You must wait **{{time}}** to be able to execute this action."
 };
 
 exports.COMMANDS = {
@@ -43,5 +67,9 @@ exports.COMMANDS = {
     MISSING_MEMBER: "You must @Mention the member to add a role.",
     MISSING_ROLE: "You must specify the role you want to grant.",
     COMPLETED: "You have added the {{role}} role to **{{user}}** ({{user.id}})."
+  },
+  CLEAR: {
+    MISSING_NUMBER_OF_MESSAGES: "You must specify the number of messages you want to purge.",
+    COMPLETED: "You have purged **{{number}}** messages from the {{channel}} channel."
   }
 };
--- Filo/es.js
+++ Filo/es.js
@@ -13,15 +13,20 @@ exports.ERROR_MESSAGES = {
   ALTERNATIVE_PERMISSION: "Permiso alternativo",
   ALTERNATIVE_PERMISSIONS: "Permisos alternativos",
   MISSING_CLIENT_PERMISSION: "Este error se debe a que no tengo el permiso [``{{permission}}``]({{url}}).",
-  MISSING_USER_PERMISSION: "Este error se debe a que no tienes el permiso [``{{permission}}``]({{url}}).",
+  MISSING_USER_PERMISSION: "Este error se debe a que no tienes el permiso ``{{permission}}``.",
   MISSING_USER_PERMISSION_2: "No tienes permisos para realizar esta acción con **{{user}}** ({{user.id}}).",
-
   INVALID_MEMBER: "El miembro que has especificado es inválido.",
   UNKNOWN_MEMBER: "El miembro que has especificado no existe o no está en el servidor.",
   INVALID_ROLE: "El rol que has especificado es inválido.",
   UNKNOWN_ROLE: "El rol que has especificado no existe.",
   MANAGED_ROLE: "El rol que has especificado está gestionado por una integración.",
-  MEMBER_ALREADY_HAS_THE_ROLE: "**{{user}}** ({{user.id}}) ya tiene el rol {{role}} asignado."
+  MEMBER_ALREADY_HAS_THE_ROLE: "**{{user}}** ({{user.id}}) ya tiene el rol {{role}} asignado.",
+  INVALID_NUMBER: "El número que has especificado es inválido.",
+  MAX_NUMBER_REACHED: "El número que has especificado es mayor de **{{number}}**.",
+  MIN_NUMBER_REACHED: "El número que has especificado es menor de **{{number}}**.",
+  INVALID_CHANNEL: "El canal que has especificado es inválido.",
+  UNKNOWN_CHANNEL: "El canal que has especificado no existe.",
+  INVALID_CHANNEL_TYPE: "Esta acción no admite el tipo del canal que has especificado. ({{valid_types}})"
 };
 
 exports.AUDIT_LOGS = {
@@ -32,10 +37,29 @@ exports.AUDIT_LOGS = {
   }
 };
 
+exports.TIME = {
+  SECOND: "Segundo",
+  SECONDS: "Segundos",
+  MINUTE: "Minuto",
+  MINUTES: "Minutos",
+  HOUR: "Hora",
+  HOURS: "Horas",
+  DAY: "Día",
+  DAYS: "Días",
+  WEEK: "Semana",
+  WEEKS: "Semanas",
+  MONTH: "Mes",
+  MONTHS: "Meses",
+  YEAR: "Año",
+  YEARS: "Años"
+};
+
 exports.GENERAL = {
   EXAMPLE: "Ejemplo",
   ADDED: "Añadido",
-  COMMAND: "Comando"
+  COMMAND: "Comando",
+  OR: "O",
+  COOLDOWN: "Debes esperar **{{time}}** para poder ejecutar esta acción."
 };
 
 exports.COMMANDS = {
@@ -43,5 +67,9 @@ exports.COMMANDS = {
     MISSING_MEMBER: "Debes @Mencionar al miembro para añadirle un rol.",
     MISSING_ROLE: "Debes especificar el rol que quieres añadir.",
     COMPLETED: "Has añadido el rol {{role}} a **{{user}}** ({{user.id}})."
+  },
+  CLEAR: {
+    MISSING_NUMBER_OF_MESSAGES: "Debes especificar el número de mensajes que quieres purgar.",
+    COMPLETED: "Has purgado **{{number}}** mensajes del canal {{channel}}."
   }
 };
